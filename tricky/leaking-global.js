2edfe9b137708744ad4ce29d91e4c080bbeca990
Valeri Vicneanschi <vicneanschi@gmail.com>
2016-01-25 23:27:53 -0500

    leaking global example

---
+++ tricky/leaking-global.js
@@ -0,0 +1,10 @@
+// uncomment this to not allow leaking global
+//'use strict';
+
+
+(function(){
+  var a = b = 3;
+})();
+
+console.log("a defined? " + (typeof a !== 'undefined'));
+console.log("b defined? " + (typeof b !== 'undefined'));
